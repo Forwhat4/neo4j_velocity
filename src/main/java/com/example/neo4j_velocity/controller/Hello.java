7ac50aeee969f2ee62e13a35e45114913a30c209
Forwhat4 <15057763977@163.com>
2018-01-05 17:05:15 +0800

    ...

--- src/main/java/com/example/neo4j_velocity/controller/Hello.java
+++ src/main/java/com/example/neo4j_velocity/controller/Hello.java
@@ -1,5 +1,8 @@
 package com.example.neo4j_velocity.controller;
 
+import com.example.neo4j_velocity.domain.User;
+import com.example.neo4j_velocity.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Hello {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/hello")
     public String sayHello(Model model){
-        model.addAttribute("name","lisi");
+
+        User user = userService.getUserByName("John Johnson");
+
+        model.addAttribute("name",user.getName());
 
         return "templates/hello";
     }
