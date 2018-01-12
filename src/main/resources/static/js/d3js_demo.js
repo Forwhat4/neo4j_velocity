090b12561066a09eaa6eeb29476e41785870f7e7
Forwhat4 <15057763977@163.com>
2018-01-12 16:52:19 +0800

    引入d3.js

--- src/main/resources/static/js/d3js_demo.js
+++ src/main/resources/static/js/d3js_demo.js
@@ -1,113 +1,126 @@
+var nodes = [ { name: "刘一"    }, { name: "陈二" },
+    { name: "张三"    }, { name: "李四"   },
+    { name: "王五"   }, { name: "赵六"    },
+    { name: "田七"    } ];
+
+var edges = [  { source : 0  , target: 1 , type : "朋友"} , { source : 0  , target: 2 , type : "朋友"} ,
+    { source : 0  , target: 3 , type : "朋友"} , { source : 1  , target: 4 , type : "朋友"} ,
+    { source : 1  , target: 5 , type : "朋友"} , { source : 1  , target: 6 , type : "朋友"} ,
+    { source : 1  , target: 0 , type : "朋友"} ];
+
+var width = 400;
+var height = 400;
 
 
-// http://blog.thomsonreuters.com/index.php/mobile-patent-suits-graphic-of-the-day/
-var links = [
-    {source: "Microsoft", target: "Amazon", type: "licensing"},
-    {source: "Microsoft", target: "HTC", type: "licensing"},
-    {source: "Samsung", target: "Apple", type: "suit"},
-    {source: "Motorola", target: "Apple", type: "suit"},
-    {source: "Nokia", target: "Apple", type: "resolved"},
-    {source: "HTC", target: "Apple", type: "suit"},
-    {source: "Kodak", target: "Apple", type: "suit"},
-    {source: "Microsoft", target: "Barnes & Noble", type: "suit"},
-    {source: "Microsoft", target: "Foxconn", type: "suit"},
-    {source: "Oracle", target: "Google", type: "suit"},
-    {source: "Apple", target: "HTC", type: "suit"},
-    {source: "Microsoft", target: "Inventec", type: "suit"},
-    {source: "Samsung", target: "Kodak", type: "resolved"},
-    {source: "LG", target: "Kodak", type: "resolved"},
-    {source: "RIM", target: "Kodak", type: "suit"},
-    {source: "Sony", target: "LG", type: "suit"},
-    {source: "Kodak", target: "LG", type: "resolved"},
-    {source: "Apple", target: "Nokia", type: "resolved"},
-    {source: "Qualcomm", target: "Nokia", type: "resolved"},
-    {source: "Apple", target: "Motorola", type: "suit"},
-    {source: "Microsoft", target: "Motorola", type: "suit"},
-    {source: "Motorola", target: "Microsoft", type: "suit"},
-    {source: "Huawei", target: "ZTE", type: "suit"},
-    {source: "Ericsson", target: "ZTE", type: "suit"},
-    {source: "Kodak", target: "Samsung", type: "resolved"},
-    {source: "Apple", target: "Samsung", type: "suit"},
-    {source: "Kodak", target: "RIM", type: "suit"},
-    {source: "Nokia", target: "Qualcomm", type: "suit"}
-];
-console.info(links);
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height);
 
-var nodes = {};
-console.info(nodes);
-// Compute the distinct nodes from the links.
-links.forEach(function(link) {
-    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-});
-
-
-
-
-var width = 500,
-    height = 200;
 
 var force = d3.layout.force()
-    .nodes(d3.values(nodes))
-    .links(links)
-    .size([width, height])
-    .linkDistance(60)
-    .charge(-300)
-    .on("tick", tick)
-    .start();
+    .nodes(nodes)		//指定节点数组
+    .links(edges)		//指定连线数组
+    .size([width,height])	//指定范围
+    .linkDistance(150)	//指定连线长度
+    .charge(-400);	//相互之间的作用力
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+force.start();	//开始作用
 
-// Per-type markers, as they don't inherit styles.
+//绘制箭头
 svg.append("defs").selectAll("marker")
-    .data(["suit", "licensing", "resolved"])
-    .enter().append("marker")
-    .attr("id", function(d) { return d; })
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 6)
-    .attr("orient", "auto")
-    .append("path")
-    .attr("d", "M0,-5L10,0L0,5");
+    .data(["end"])
+    .enter()
+    .append("svg:marker")
+    .attr("id","arrow")   //连线可以根据id添加标识
+    //.attr('class','arrow')
+    //.attr("viewBox", "0 -5 10 10")   	//坐标系的区域
+    .attr("viewBox", "0 0 12 12")
+    .attr("refX", 20)  //标识坐标
+    .attr("refY", 6)
+    .attr("markerWidth", 20)    //标识大小
+    .attr("markerHeight", 16)
+    .attr("markerUnits","userSpaceOnUse")   //标识大小的基准，有两个值：strokeWidth（线的宽度）和userSpaceOnUse（图形最前端的大小）
+    .attr("orient", "auto")  //绘制方向，可设定为：auto（自动确认方向）和 角度值
+    .append("svg:path")
+    //.attr("d", "M0,-5L10,0L0,5")  //标识的路径
+    .attr("d", "M2,2 L10,6 L2,10 L6,6 L2,2")
+    .attr('fill','green'); //填充颜色
 
-var path = svg.append("g").selectAll("path")
-    .data(force.links())
-    .enter().append("path")
-    .attr("class", function(d) { return "link " + d.type; })
-    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
 
-var circle = svg.append("g").selectAll("circle")
-    .data(force.nodes())
-    .enter().append("circle")
-    .attr("r", 6)
-    .call(force.drag);
 
-var text = svg.append("g").selectAll("text")
-    .data(force.nodes())
-    .enter().append("text")
-    .attr("x", 8)
-    .attr("y", ".31em")
-    .text(function(d) { return d.name; });
+//添加连线
+var svg_edges = svg.selectAll("line")
+    .data(edges)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .attr('stroke-width',1)  //线条粗细
+    .attr("marker-end","url(#arrow)") //添加箭头  marker-start ：路径起点处  marker-mid：路径中间端点处（必须是 path 中间出现的点）  marker-end ：路径终点处
+    .attr('stroke','#999');   //描边颜色
 
-// Use elliptical arc path segments to doubly-encode directionality.
-function tick() {
-    path.attr("d", linkArc);
-    circle.attr("transform", transform);
-    text.attr("transform", transform);
-}
 
-function linkArc(d) {
-    var dx = d.target.x - d.source.x,
-        dy = d.target.y - d.source.y,
-        dr = Math.sqrt(dx * dx + dy * dy);
-    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-}
+var color = d3.scale.category20();
 
-function transform(d) {
-    return "translate(" + d.x + "," + d.y + ")";
-}
+//节点拖拽、固定
+var drag = force.drag()
+    .on("dragstart",function(d,i){
+        d.fixed = true;    //拖拽开始后设定被拖拽对象为固定
+        //label_text_2.text("拖拽状态：开始");
+    })
+
+//添加节点
+var svg_nodes = svg.selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("r",10)
+    .style("fill",function(d,i){
+        return color(i);
+    })
+    .call(drag);	//使得节点能够拖动
+
+//添加描述节点的文字
+var svg_texts = svg.selectAll("text")
+    .data(nodes)
+    .enter()
+    .append("text")
+    .style("fill", "black")
+    .attr("dx", -15)
+    .attr("dy", 30)
+    .text(function(d){
+        return d.name;
+    });
+
+//添加描述连线的文字
+var edges_text = svg.selectAll(".linetext")
+    .data(edges)
+    .enter()
+    .append("text")
+    .attr("class","linetext")
+    .text(function(d){
+        return d.type;
+    });
+
+
+force.on("tick", function(){	//对于每一个时间间隔
+
+    //更新连线坐标
+    svg_edges.attr("x1",function(d){ return d.source.x; })
+        .attr("y1",function(d){ return d.source.y; })
+        .attr("x2",function(d){ return d.target.x; })
+        .attr("y2",function(d){ return d.target.y; });
+
+    //更新节点坐标
+    svg_nodes.attr("cx",function(d){ return d.x; })
+        .attr("cy",function(d){ return d.y; });
+
+    //更新文字坐标
+    svg_texts.attr("x", function(d){ return d.x; })
+        .attr("y", function(d){ return d.y; });
+
+    //更新连接线上文字的位置
+    edges_text.attr("x",function(d){ return (d.source.x + d.target.x) / 2 ; });
+    edges_text.attr("y",function(d){ return (d.source.y + d.target.y) / 2 ; });
+});
 
